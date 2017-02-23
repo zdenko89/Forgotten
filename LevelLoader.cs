819cc843f8a986b8a88f1293dc5be194d2ff427f
Daniel <dsan.work@gmail.com>
2017-02-23 20:36:19 +0000

    added the level loader

---
+++ LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour {
+
+	private bool playerInZone;
+
+	public string levelToLoad;
+
+	void Start () 
+	{
+		playerInZone = false;
+	}
+
+	void Update () 
+	{
+		if (playerInZone) 
+		{
+			Application.LoadLevel (levelToLoad);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player") 
+		{
+			playerInZone = true;
+		}
+	}
+}
